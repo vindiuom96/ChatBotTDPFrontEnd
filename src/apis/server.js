8c542e0061992892aad009d252b31a39f017bd4d
Vindya Perera <40080989+vindiuom96@users.noreply.github.com>
2023-05-21 15:51:53 +1000

    Update server.js

--- src/apis/server.js
+++ src/apis/server.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios  from "axios";
 
-const version1_url = "http://ec2-52-55-166-140.compute-1.amazonaws.com:5000";
-const version3_url = "http://ec2-18-210-7-47.compute-1.amazonaws.com:5000";
-const localhost = "http://localhost:5000";
+const version1_url = "http://ec2-52-55-166-140.compute-1.amazonaws.com:5000"
+const version3_url = "http://ec2-3-237-252-85.compute-1.amazonaws.com:5000"
+const localhost = "http://localhost:5000"
 export default axios.create({
-  baseURL: localhost,
-});
+    baseURL: version3_url,
+  });
