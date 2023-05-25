067a3081df076abb1da481515c2620190bc18546
Vindya Perera <40080989+vindiuom96@users.noreply.github.com>
2023-05-25 13:45:40 +1000

    Delete src directory

--- src/apis/server.js
+++
@@ -1,8 +0,0 @@
-import axios  from "axios";
-
-const version1_url = "http://ec2-52-55-166-140.compute-1.amazonaws.com:5000"
-const version3_url = "http://ec2-3-237-252-85.compute-1.amazonaws.com:5000"
-const localhost = "http://localhost:5000"
-export default axios.create({
-    baseURL: version3_url,
-  });
